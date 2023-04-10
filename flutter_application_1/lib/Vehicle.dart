8622edb7f7e92c776c238e1e822fb5106cea6964
AxelTingman <axeltingman@axeltingman.net>
2023-04-10 23:29:34 +0200

    basic classes

--- flutter_application_1/lib/Vehicle.dart
+++ flutter_application_1/lib/Vehicle.dart
@@ -1,5 +1,10 @@
+import 'package:flutter_application_1/VehicleBrand.dart';
+import 'package:flutter_application_1/VehicleModel.dart';
+
+
 class Vehicle {
-  String id = "";
+  String id = "[no ID]";
+  VehicleModel model = VehicleModel(VehicleBrand("[no name]"), "[no name]");
 
   Vehicle (String id) {
     this.id = id;
