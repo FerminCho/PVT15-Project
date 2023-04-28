081012ed179c8f4bae07b05caad456ab29e26dcc
FerminCho <fermin.liljeqvist@gmail.com>
2023-04-28 13:37:22 +0200

    added values for the user class

--- google_maps_in_flutter/lib/user.dart
+++ google_maps_in_flutter/lib/user.dart
@@ -1,10 +1,12 @@
 
 class User{
   String? name;
-  var login;
   String? database;
   int? score;
   var settings;
+  var accesstoken;
+  var profile;
+
 
   void nameSetter(String name){
     this.name = name;
